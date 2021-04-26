dbae8ddb41bda477e57662182f5fb1520dddedd5
Raj Kamaria <raj.kamaria@kellogg.northwestern.edu>
2021-04-26 11:03:11 -0500

    Done

--- noober.js
+++ noober.js
@@ -1,7 +1,7 @@
 window.addEventListener('DOMContentLoaded', async function() {
   let response = await fetch('https://kiei451.com/api/rides.json')
   let json = await response.json()
-
+}
   // writes the returned JSON to the console
   console.dir(json)
   
